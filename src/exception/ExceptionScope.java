c819c49650f842e70f00bc32015d9d96f2bbdb79
Hana <hanaindec@gmail.com>
2022-04-20 11:40:46 +0900

    exercise exception

---
+++ src/exception/ExceptionScope.java
@@ -0,0 +1,32 @@
+package exception;
+
+public class ExceptionScope {
+
+    //Catches and handles the exception that is thrown in level3.
+    public void level1() {
+        System.out.println("Level 1 beginning.");
+        try {
+            level2();
+        } catch (ArithmeticException e) {
+            System.out.println("\nThe exception msg is: "+ e.getMessage());
+            System.out.println("The call stack trace: ");
+            e.printStackTrace();
+            System.out.println();
+        }
+        System.out.println("Level 1 ending.");
+    }
+
+    public void level2() {
+        System.out.println("Level 2 beginning.");
+        level3();
+        System.out.println("Level 2 ending.");
+    }
+
+    public void level3() {
+        int numerator = 10, denominator = 0;
+        System.out.println("Level 3 beginning.");
+        int result = numerator/denominator;
+        System.out.println("Level 3 ending.");
+    }
+
+}
---
+++ src/exception/Propagation.java
@@ -0,0 +1,14 @@
+package exception;
+
+//Demonstrates exception propagation.
+public class Propagation {
+    //Invokes the level1 method to begin the exception demonstration.
+    public static void main(String[] args) {
+        ExceptionScope demo = new ExceptionScope();
+        System.out.println("Program beginning.");
+        demo.level1();
+        System.out.println("Program ending.");
+
+    }
+
+}
